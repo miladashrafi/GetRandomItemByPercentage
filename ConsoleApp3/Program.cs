98a877ef96911bb41beb385e72f7f1c392a9d27a
mi.ashrafi <mi.ashrafi@alibaba.ir>
2022-10-23 00:09:16 +0330

    fix

--- ConsoleApp3/Program.cs
+++ ConsoleApp3/Program.cs
@@ -1,15 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-
+﻿
 int onePercentage = 30, twoPercentage = 70;
-
-var list = Enumerable.Repeat("One", onePercentage).ToList();
-list.AddRange(Enumerable.Repeat("Two", twoPercentage));
 
 var all = new List<string>();
 
 while (true)
 {
-    var item = list.OrderBy(x => Guid.NewGuid()).First();
+    var x = Random.Shared.Next(1, 100);
+    var item = onePercentage > x ? "One" : "Two";
+
     all.Add(item);
     var items = all.GroupBy(x => x).Select(x => new { x.Key, Count = x.Count() });
 
@@ -18,9 +16,8 @@ while (true)
 
     Console.WriteLine($"Set-points => One: {onePercentage}% / Two: {twoPercentage}% | " +
         $"Current: {item} | " +
-        $"Percentage => One: {one * 100 / all.Count}% / Two: {two * 100 / all.Count}% | " +
+        $"Actual Percentage => One: {one * 100 / all.Count}% / Two: {two * 100 / all.Count}% | " +
         $"Total: {all.Count}");
     Thread.Sleep(Random.Shared.Next(50, 500));
     Console.Clear();
 }
-
